9df74bf03a5c0f3b72f92ab072ed59d2a6e2b5d3
Yeskenderkhan <253929@astanait.edu.kz>
2026-01-19 20:21:12 +0500

    Student p1

---
+++ src/unienroll/Student.java
@@ -0,0 +1,13 @@
+package unienroll;
+
+public class Student {
+    private int id;
+    private String fullName;
+    private String email;
+
+    public Student(int id, String fullName, String email) {
+        this.id = id;
+        this.fullName = fullName;
+        this.email = email;
+    }
+}
