9d7b2f7e7fa4248061752fc0bf0acfec563c211e
adilek <250614@astanait.edu.kz>
2026-01-21 23:29:00 +0500

    student entity added

---
+++ src/unienroll/entity/Course.java
@@ -0,0 +1,6 @@
+package unienroll.entity;
+
+public class Course {
+    private int id;
+    private String name;
+}
---
+++ src/unienroll/entity/Registration.java
@@ -0,0 +1,7 @@
+package unienroll.entity;
+
+public class Registration {
+    private int id;
+    private int studentId;
+    private int courseId;
+}
