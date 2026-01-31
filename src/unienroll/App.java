81a4f2b8271809f839ab1b130fba2c778e91ee61
Yeskenderkhan <253929@astanait.edu.kz>
2026-01-31 20:01:52 +0500

    changes

--- src/unienroll/App.java
+++ src/unienroll/App.java
@@ -1,7 +1,12 @@
 package unienroll;
 
+import unienroll.ui.Console;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("UniEnroll application started");
+
+        Console console = new Console();
+        console.start();
     }
 }
--- src/unienroll/ui/Console.java
+++ src/unienroll/ui/Console.java
@@ -4,5 +4,9 @@ public class Console {
 
     public void start() {
         System.out.println("Console UI started");
+
+        // тут будет меню
+        // тут Scanner
+        // тут вызовы Service
     }
 }
